2f542dc09ae3e7ca3d9c7228a53a40160245480c
GRC <gcutaiar@yahoo.com>
2019-10-31 20:52:34 -0500

    sql tables created

---
+++ Happiness DB/Tables.sql
@@ -0,0 +1,30 @@
+-- CREATE TABLE suicide_alcohol_full (
+-- 	country VARCHAR(255),
+-- 	average DECIMAL,
+-- 	female DECIMAL, 
+-- 	male DECIMAL,
+-- 	region VARCHAR(255),
+-- 	hemisphere VARCHAR(255),
+-- 	happiness VARCHAR(255),
+-- 	hdi DECIMAL,
+-- 	gdp_percapita DECIMAL,
+-- 	beer_percapita DECIMAL,
+-- 	spirit_percapita DECIMAL,
+-- 	wine_percapita DECIMAL
+-- );
+-- SElECT * FROM suicide_alcohol_full;
+-- CREATE TABLE suicide_alcohol_reduced (
+-- 	country VARCHAR(255),
+-- 	average DECIMAL,
+-- 	female DECIMAL, 
+-- 	male DECIMAL,
+-- 	region VARCHAR(255),
+-- 	hemisphere VARCHAR(255),
+-- 	happiness VARCHAR(255),
+-- 	hdi DECIMAL,
+-- 	gdp_percapita DECIMAL,
+-- 	beer_percapita DECIMAL,
+-- 	spirit_percapita DECIMAL,
+-- 	wine_percapita DECIMAL
+-- );
+SElECT * FROM suicide_alcohol_reduced;
